b696f1e2c5d1e04c5c4ae934f2744d82e4ffcdb9
Christian Morales <cmorale1@trinity.edu>
2019-02-07 17:34:19 -0600

    Is this gonna work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from './app/containers/Login/Login';
 import AllCandidates from './app/containers/AllCandidatesView/AllCandidates';
 import SignUp from './app/containers/SignUp/SignUp';
