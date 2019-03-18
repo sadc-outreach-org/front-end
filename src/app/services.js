e2f8123eb2466009fb15c663004a66407c8327ec
Gerardo Gonzalez <32018140+ggonzal2@users.noreply.github.com>
2019-03-18 18:24:06 -0500

    updated services

--- src/app/services.js
+++ src/app/services.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
 const API_URL = "http://34.73.221.154:8080";
+var user = "1";
+var site = "http://34.73.221.154:8080/users/";
+
 
 export const getUsers = () => {
     return axios.get(`${API_URL}/user/users`);
@@ -13,3 +16,13 @@ export const addCandidate = (payload) => {
 export const login = (payload) => {
     return axios.post(`${API_URL}/user/login`, payload);
 }
+export const getResume = () => {
+    return  axios(site.concat(user,"/resume"), {
+        method: 'GET',
+        responseType: 'blob' //Force to receive data in a Blob Format
+    })
+    //axios.get(site.concat(user,"/resume"))
+};
+export const uploadResume = (pdfFile) => {
+    return axios.post(site.concat(user,"/resume"), pdfFile);
+};
