7ee6003b594fed75a5ea6e8f22c337792c9f3a86
Christian Morales <cmorale1@trinity.edu>
2019-03-18 19:49:03 -0500

    Updated index.js to point to App instead of Signup

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
-import SignUp from './app/containers/SignUp/SignUp';
+// import SignUp from './app/containers/SignUp/SignUp';
 import * as serviceWorker from './serviceWorker';
 
-// ReactDOM.render(<App/>, document.getElementById('root'));
-ReactDOM.render(<SignUp/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
+// ReactDOM.render(<SignUp/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
