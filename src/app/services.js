6d2dc1a94c6a3270807d569a540fb3049d709a36
Gerardo Gonzalez <32018140+ggonzal2@users.noreply.github.com>
2019-03-21 10:49:51 -0500

    tested some thumbnail did not work

--- src/app/services.js
+++ src/app/services.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-
 const API_URL = "http://34.73.221.154:8080";
 var user = "1";
 var site = "http://34.73.221.154:8080/users/";
@@ -7,17 +6,18 @@ var site = "http://34.73.221.154:8080/users/";
 
 export const getUsers = () => {
     return axios.get(`${API_URL}/user/users`);
-}
+};
 
 export const addCandidate = (payload) => {
     return axios.post(`${API_URL}/user/signup`, payload);
-}
+};
 
 export const login = (payload) => {
     return axios.post(`${API_URL}/user/login`, payload);
-}
+};
 export const getResume = () => {
-    return  axios(site.concat(user,"/resume"), {
+    //http://34.73.221.154:8080/users/1/resume
+    return  axios('http://34.73.221.154:8080/users/1/resume', {
         method: 'GET',
         responseType: 'blob' //Force to receive data in a Blob Format
     })
