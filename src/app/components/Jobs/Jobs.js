ad30feb5723db3c3be861cfcdbe688ec5b82a06f
Christian Morales <cmorale1@trinity.edu>
2019-03-25 17:53:16 -0500

    Added ability for user to filter requisitions by job title

--- src/app/components/Jobs/Jobs.js
+++ src/app/components/Jobs/Jobs.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../../../styles/AllJobs.css';
-import {getJobs, getReqsForJob} from '../../services.js';
+import {getJobs, getRequisitions, getReqsForJob} from '../../services.js';
 import { Link } from 'react-router-dom';
 
 export default class Jobs extends React.Component {
@@ -12,20 +12,35 @@ export default class Jobs extends React.Component {
         };
 
         this.handleJobClick = this.handleJobClick.bind(this);
+        this.handleResetClick = this.handleResetClick.bind(this);
     }
 
     handleJobClick(jobID) {
         console.log(jobID + " was clicked");
+
         getReqsForJob(jobID).then(res => {
             const reqs = res.data.result;
+            this.setState({reqs: reqs})
             console.log(reqs);
+        })
+    };
+
+    handleResetClick() {
+        getRequisitions().then(res => {
+            const reqs = res.data.result;
+            this.setState({reqs: reqs})
         })
     };
 
     componentDidMount() {
         getJobs().then(res => {
             const jobs = res.data.result;
-            this.setState({jobs});
+            this.setState({jobs: jobs});
+        });
+
+        getRequisitions().then(res => {
+            const reqs = res.data.result;
+            this.setState({reqs: reqs});
         })
     };
 
@@ -34,6 +49,7 @@ export default class Jobs extends React.Component {
             <div className={"jobContainer"}>
                 <h1 id={"jobsHeader"}>Jobs</h1>
                 <input type={"text"} id={"searchInput"} onKeyUp={console.log("typed")} placeholder={"Search by title"} title={"Type in a title"}/>
+                <button className={"resetButton"} onClick={() => this.handleResetClick()}>Reset</button>
                 <table>
                     <tbody>
                     <tr>
@@ -50,6 +66,21 @@ export default class Jobs extends React.Component {
                             </td>
                         </tr>)
                     }
+                    </tbody>
+                </table>
+                <br/>
+                <table>
+                    <tbody>
+                    <tr>
+                        <th>Requisition</th>
+                    </tr>
+                    {this.state.reqs.map(req =>
+                        <tr>
+                            <td>
+                                {req.title}
+                            </td>
+                        </tr>
+                    )}
                     </tbody>
                 </table>
             </div>
