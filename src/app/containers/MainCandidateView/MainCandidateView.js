e40d36571ecc9e53f0cba55311023f9867865af3
kmenzel <kmenzel@trinity.edu>
2019-03-26 11:54:14 -0500

    added notification menu item, adding the modal returned errors

--- src/app/containers/MainCandidateView/MainCandidateView.js
+++ src/app/containers/MainCandidateView/MainCandidateView.js
@@ -6,24 +6,50 @@ import SideMenu, {Item} from 'react-sidemenu';
 import CandidateProfileInfo from '../../components/CandidateProfileInfo/CandidateProfileInfo';
 import Applications from '../../components/Applications/Applications';
 import {Route, HashRouter} from "react-router-dom";
+import {Modal} from 'react-modal';
+import ApplicationStatus from "../../components/ApplicationStatus/ApplicationStatus";
 
 class MainCandidateView extends Component {
+    constructor () {
+        super();
+        this.state = {
+            showModal: false
+        };
+
+        this.handleOpenModal = this.handleOpenModal.bind(this);
+        this.handleCloseModal = this.handleCloseModal.bind(this);
+    }
+
     render() {
         return(
-            <HashRouter>
-                <div className={"profileSideNavComponent"}>
-                    <SideMenu>
-                        <Item divider={true} label="Actions" value="segment1"/>
-                        <Item label="Profile" value="/profile"></Item>
-                        <Item label="Application Status" value="/applications"></Item>
-                    </SideMenu>
-                    <div className={"main-candidate-content"}>
-                        <Route path={"/profile"} component={CandidateProfileInfo}/>
-                        <Route path={"/applications"} component={Applications}/>
+            <div>
+                <HashRouter>
+                    <div className={"profileSideNavComponent"}>
+                        <SideMenu>
+                            <Item divider={true} label="Actions" value="segment1"/>
+                            <Item label="Profile" value="/profile"></Item>
+                            <Item label="Application Status" value="/applications"></Item>
+                            <Item divider={true} label={"Notifications"}/>
+                            <Item label={"Notifications"} onClick={this.handleOpenModal}/>
+                        </SideMenu>
+                        <div className={"main-candidate-content"}>
+                            <Route path={"/profile"} component={CandidateProfileInfo}/>
+                            <Route path={"/applications"} component={Applications}/>
+                        </div>
                     </div>
-                </div>
-            </HashRouter>
+
+                </HashRouter>
+
+            </div>
         )
+    }
+
+    handleOpenModal(){
+        this.setState({showModal: true})
+    }
+
+    handleCloseModal(){
+        this.setState({showModal: false})
     }
 }
 
