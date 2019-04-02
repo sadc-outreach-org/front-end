24a43d7abe7eac235bbeda658413428a6c2ac253
Christian <cmorale1@trinity.edu>
2019-04-02 12:23:07 -0500

    Small CSS fixes

--- src/index.js
+++ src/index.js
@@ -2,15 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
-import MainCandidateView from './app/containers/MainCandidateView/MainCandidateView';
-import MainHiringManagerView from './app/containers/MainHiringManagerView/MainHiringManagerView';
-import Login from './app/containers/Login/Login';
 import * as serviceWorker from './serviceWorker';
 
-//ReactDOM.render(<App/>, document.getElementById('root'));
-//ReactDOM.render(<Login/>, document.getElementById('root'));
-ReactDOM.render(<MainCandidateView/>, document.getElementById('root'));
-//ReactDOM.render(<MainHiringManagerView/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
