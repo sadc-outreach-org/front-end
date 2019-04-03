5b8e03416a0d25d0e11ebe093c130f0c4ac5989a
Christian <cmorale1@trinity.edu>
2019-04-03 12:21:38 -0500

    Small CSS fix, WIP: Add Req to Candidate

--- src/app/components/CandidateProfileModal/CandidateProfileModal.js
+++ src/app/components/CandidateProfileModal/CandidateProfileModal.js
@@ -2,17 +2,44 @@ import React from 'react';
 import '../../../styles/CandidateProfileModal.css';
 
 export default class ApplicationStatus extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showAddToReq: false,
+            buttonText1: "Send Email",
+            buttonText2: "Assign to New Requisition"
+        };
+
+        this.handleAddToReqClick = this.handleAddToReqClick.bind(this);
+        this.handleCancelClick = this.handleCancelClick.bind(this);
+    }
+
+    handleAddToReqClick () {
+        this.setState({buttonText1: "Cancel"});
+        this.setState({buttonText2: "Assign"});
+        this.setState({showAddToReq: true});
+    }
+
+    handleCancelClick () {
+        if(this.state.showAddToReq === true) {
+            this.setState({showAddToReq: false});
+            this.setState({buttonText1: "Send Email"});
+            this.setState({buttonText2: "Assign to New Requisition"});
+        }
+    }
 
     render() {
         return (
             <div className={"candidateProfileModalContainer"}>
                 <h1>{this.props.info.firstName + " " + this.props.info.lastName}</h1>
-                <p><strong>Email:</strong>{" " + this.props.info.email}</p>
+                <p><strong>Email: </strong><a href={"mailto:"+this.props.info.email}>{this.props.info.email}</a></p>
                 <p><strong>Phone:</strong>{" " + this.props.info.phoneNum}</p>
                 <p><strong>Address:</strong>{" " + this.props.info.streetAddress}</p>
                 <p><strong>Location:</strong>{" " + this.props.info.city + ", " + this.props.info.state}</p>
+                <p>Conditional Text!</p>
                 <div className={"candidateProfileModalButtons"}>
-                    <button id={"candidateProfileModalButton1"}>Send Email</button><button id={"candidateProfileModalButton2"}>Assign to Requisition</button>
+                    <button id={"candidateProfileModalButton1"} onClick={this.handleCancelClick}><a href={"mailto:"+this.props.info.email}>{this.state.buttonText1}</a></button>
+                    <button id={"candidateProfileModalButton2"} onClick={this.handleAddToReqClick}>{this.state.buttonText2}</button>
                 </div>
             </div>
         )
