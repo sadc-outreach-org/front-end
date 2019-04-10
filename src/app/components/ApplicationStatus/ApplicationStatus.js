c0ce648e554fb6d5defa8df80fa4b6d120c77358
Christian Morales <cmorale1@trinity.edu>
2019-04-10 14:53:08 -0500

    Added conditional classes for the list of steps

--- src/app/components/ApplicationStatus/ApplicationStatus.js
+++ src/app/components/ApplicationStatus/ApplicationStatus.js
@@ -7,7 +7,8 @@ export default class ApplicationStatus extends React.Component {
     constructor (props) {
         super(props);
         this.state = {
-            gitLink: ''
+            gitLink: '',
+            showUpdated: false
         };
 
         this.handleGitSubmit = this.handleGitSubmit.bind(this);
@@ -15,6 +16,13 @@ export default class ApplicationStatus extends React.Component {
 
     componentDidMount() {
         this.setState({gitLink: this.props.clickedApplication.gitLink});
+        let steps = document.getElementsByClassName("progressbar-item");
+        let i;
+        for(i = 0; i < steps.length; i++) {
+            if(steps[i].innerHTML.includes(this.props.clickedApplication.status)) {
+                steps[i].className = steps[i].className+" active";
+            }
+        }
     }
 
     handleGitSubmit = event => {
@@ -22,10 +30,8 @@ export default class ApplicationStatus extends React.Component {
         let payload = {
             gitLink: this.state.gitLink
         };
-
-        submitGitLink(payload, this.props.clickedApplication.applicationID).then(res => {
-            console.log("Github Submit Response: " + JSON.stringify(res));
-        })
+        submitGitLink(payload, this.props.clickedApplication.applicationID);
+        this.setState({showUpdated: true});
     };
 
     render() {
@@ -33,20 +39,19 @@ export default class ApplicationStatus extends React.Component {
             <div className={"applicationStatusContainer"}>
                 <h1 className={"application-header"}>Application Status for: {this.props.clickedApplication.requisition.title}</h1>
                 <ul className="progressbar">
-                    <li className="active">Coding Challenge</li>
-                    <li className="active">Submit Code for Review</li>
-                    <li>Attend Interview</li>
+                    <li className={"progressbar-item"}>Submit Code for Review</li>
+                    <li className={"progressbar-item"}>Schedule Interview</li>
+                    <li className={"progressbar-item"}>Attend Interview</li>
+                    <li className={"progressbar-item"}>Wait for Response</li>
                 </ul>
-                <h2 className={"application-subheader"}>Step 1: Coding Challenge</h2>
+                <h2 className={"application-subheader"}>Step 1: Submit Code for Review</h2>
                 <p className={"application-p"}>Coding Challenge: Write a program that prints out "Hello World!"</p>
-                <h2 className={"application-subheader"}>Step 2: Submit Code for Review</h2>
-                <p className={"application-p"}>Once you've completed the coding challenge, submit your github link here:</p>
                 <Form onSubmit={this.handleGitSubmit}>
                     <FormGroup>
                         <Input
                             type={"text"}
                             name={"gitHubSubmitField"}
-                            id={"gitHubSubmitField"}
+                            id={"githubSubmissionLink"}
                             placeholder={"Github Repo Link"}
                             defaultValue={this.props.clickedApplication.gitLink}
                             onChange={(event) => this.setState({gitLink: event.target.value})}
@@ -54,12 +59,19 @@ export default class ApplicationStatus extends React.Component {
                         />
                         <Button
                             type={"submit"}
-                            className={"submitGitLink btn-block"}
+                            className={"gitHubSubmissionButton btn-block"}
                         >Submit</Button>
                     </FormGroup>
                 </Form>
+                <h2 className={"application-subheader"}>Step 2: Schedule Interview</h2>
+                <p className={"application-p"}>Waiting for Hiring Manager to schedule your interview</p>
                 <h2 className={"application-subheader"}>Step 3: Attend Interview</h2>
                 <p className={"application-p"}>Your interview is scheduled for February 2, 2019 at 3:00 PM.</p>
+                <h2 className={"application-subheader"}>Step 4: Wait for Response</h2>
+                <p className={"application-p"}>Please allow at least two weeks for a response from your hiring manager.</p>
+                <div hidden={!this.state.showUpdated} className={"applicationUpdated"}>
+                    <p><strong>Your Application has been successfully updated.</strong></p>
+                </div>
             </div>
         )
     }
